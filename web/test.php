bb006f31326a63bfa1f604dd562dcfacdd6c274a
Kuo-Kai Chin <kuokai.chin@gmail.com>
2013-02-08 17:58:31 -0500

    new mvc test

---
+++ web/test.php
@@ -0,0 +1,9 @@
+<?
+
+    // require common code
+    require_once("../../includes/common.php");
+    
+	// render a page
+	render("index.php", array("title" => "Harvard College Alzheimer's Buddies"));
+
+?>
