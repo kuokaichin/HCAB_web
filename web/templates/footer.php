e4742f7433983efa224b6ce8fbfe957622025422
Kuo-Kai Chin <kuokai.chin@gmail.com>
2013-02-08 17:53:31 -0500

    header footer

---
+++ web/templates/footer.php
@@ -0,0 +1,19 @@
+</div>
+
+                <div class="footer">
+                Designed by a Rennix?
+                        <div class="right">
+                        &#169;
+                        Harvard College Alzheimer's Buddies
+                        </div>
+                </div>
+                <br />
+                <br />
+                <br />
+
+
+
+	
+	 </body>
+</html>
+
---
+++ web/templates/header.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en">
+        <head>
+                <meta charset="utf-8">
+<meta name="google-site-verification" content="QIrAmtihNVke6kjNeodWYQcTq2BrGHXL4DG6VI230ig" />
+                <title>Alzheimer's Buddies at Harvard College</title>
+                <link rel="stylesheet" href="nav/css/nav.css">
+                <link rel="stylesheet" href="HCAB/css/hcabstyles.css">
+                <!--[if IE]>
+                        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+                <![endif]-->
+        </head>
+        <body class="no-js">
+            <header2>
+                <img class="size" src="HCAB/images/logo-horiz2wblue.png" width="1359" height="214"/>
+            </header2>
+
+                <nav id="topNav">
+                        <ul>
+                                <li><a href="index.html" title="Home"><w>Home</w></a></li>
+                                <li><a href="#" title="About us"><wu>About Us</wu><ul>
+                                                <li><a href="aboutus.html" title="About Us"><ws>Overview</ws></a></li>
+                                                <li><a href="history.html" title="History"><ws>History</ws></a></li>
+						<li><a href="leader.html" tite="Our Leaders"><ws>Our Leaders</ws></a></li>
+                                                <li class="last"><a href="buddystories.html" title="Buddy Stories"><ws>Buddy 
+Stories</ws></a></li>
+                                        </ul>
+                                </li>
+                                <li><a href="construction.html" title="Media"><w>Media</w></a></li>
+                                <li><a href="joinschedule.html" title="Join Us"><w>Join Us</w></a></li>
+                                <li><a href="contact.html" title="Contact Us"><w>Contact Us</w></a></li>
+                                <li><a href="donate.html" title="Donate"><w>Donate</w></a></li>
+                                <li><a href="HCAB/login.html" title="Login"><w>Login</w></a></li>
+                        </ul>
+                </nav>
+                <script src="nav/js/jquery.js"></script>
+                <script src="nav/js/modernizr.js"></script>
